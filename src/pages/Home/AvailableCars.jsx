25b0e799ae1285285bb63e6af348353859a446fe
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-24 13:21:49 +0600

    testing

--- src/pages/Home/AvailableCars.jsx
+++ src/pages/Home/AvailableCars.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AvailableCars = () => {
+    const [cars, setCars] = useState([]);
+    // useEffect(()=>{
+    //      fetch('http://localhost:7000/cars')
+    //      .then(res=>res.json())
+    //      .then(data=>{
+    //         setCars(data);
+    //      })
+    // },[])
+    useEffect(()=>{
+        fetch('https://jit-car-rental-server.vercel.app/cars')
+        .then(res=>res.json())
+        .then(data=>{
+           setCars(data);
+        })
+   },[])
     return (
         <div>
-            <h2>This page is updating....</h2>
+            <h2 className='text-2xl text-center font-bold'>Available Cars: {cars.length} </h2>
+            
         </div>
     );
 };
