9c880f7938dfd920d04b617ee801d126d52b8ae9
indroji-mondal <indro.cse.bu@gmail.com>
2025-03-27 16:11:49 +0600

    I am back

--- src/components/AvailableCarCard.jsx
+++ src/components/AvailableCarCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const AvailableCarCard = () => {
+const AvailableCarCard = ({image}) => {
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-xl">
                 <figure>
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        src={image}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
--- src/pages/Home/AvailableCars.jsx
+++ src/pages/Home/AvailableCars.jsx
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import AvailableCarCard from '../../components/AvailableCarCard';
+import axios from 'axios';
 
 const AvailableCars = () => {
     const [cars, setCars] = useState([]);
-   
+
+    // useEffect(() => {
+    //     fetch('https://jit-car-rental-server.vercel.app/cars')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setCars(data);
+    //         })
+    // }, [])
+    const [loading, setLoading] = useState(true); // Loader state
+
     useEffect(() => {
-        fetch('https://jit-car-rental-server.vercel.app/cars')
-            .then(res => res.json())
-            .then(data => {
-                setCars(data);
+        axios.get("https://jit-car-rental-server.vercel.app/cars")
+            .then((res) => {
+                setCars(res.data);
+                setLoading(false); // Set loading to false once data is fetched
             })
-    }, [])
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false); // Stop loading even if an error occurs
+            });
+    }, []);
+
     return (
         <div className='w-11/12 lg:w-10/12 mx-auto pt-5'>
-            <h2 className='text-2xl text-center font-bold'>Available Cars: {cars.length} </h2>
-            <div className=' grid grid-cols-1 md:grid-cols-2 gap-4  lg:grid-cols-3 '>
-                {
-                    cars.map(car=><AvailableCarCard key={car._id}></AvailableCarCard>)
-                }
-            </div>
+
+            {loading ? 'Loading' : <>
+                <h2 className='text-2xl text-center font-bold'>Available Cars: {cars.length} </h2>
+
+                <div className=' grid grid-cols-1 md:grid-cols-2 gap-4  lg:grid-cols-3 '>
+                    {
+                        cars.map(car => <AvailableCarCard image={car.image} key={car._id}></AvailableCarCard>)
+                    }
+                </div>
+            </>
+            }
+
         </div>
     );
 };
